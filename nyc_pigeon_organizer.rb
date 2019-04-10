d2c6f7659240ab6c1d906b47a681c74b1cdd88df
Charlotte Goldthwaite <chargold201@gmail.com>
2019-04-10 20:37:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, options|
+    options.each do |option, names|
+      names.each do |name|
+        if pigeon_list.has_key?(name) == false
+          pigeon_list[name] = {}
+          binding.pry
+        end
+      end
+    end
+  end
 end
